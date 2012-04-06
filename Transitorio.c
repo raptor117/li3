fc3f575937805169113c54815dfe2d18a4a7d232
Rogério <r06er62@gmail.com>
2012-04-06 23:16:17 +0100

    gerador de servicos random

--- Transitorio.c
+++ Transitorio.c
@@ -27,7 +27,6 @@ int loadData(Trans *dest){
 	valor=load_camioes(&(*dest)->cmz,"trucks.txt");
 	valor=loadcity_trucks((*dest)->cidz,(*dest)->cmz,"trucks.txt");/*este tem que ser chamado depois de
 	                                                                a load camioes e load cidades ja foi chamada*/
-	printf("%d\n",valor);
 	return valor;
 }
 
@@ -70,7 +69,87 @@ int realiza_movimento(Trans trs,char *origem,char *dest,char *contrib){
 	return valor;
 }
 
+/*Funcao random para atribuir servicos a clientes
+int atribServico(Trans dest) {
+    int y=0,l=0;
+	char citis[(int)dest->cidz->cidades->tsize][MAXS];
+	char users[(int)dest->cidz->cidades->tsize][MAXS];
+	/*for(y=0;y<(int)dest->cidz->cidades->tsize;y++){
+	    for(l=0;l<MAXS;l++){
+	        users[y][l]='\0';
+	    ;}
+	
+	}*//*
+	char *buffer,*b;
+	buffer=malloc(200*sizeof(char));
+	int i=0,j,valor,o=0,x,c,v,z;
+	Nodo_ll aux;
+	b=buffer;
+	FILE *f,*h,*g;
+	char nome[MAXS];
 
+		f=fopen("cities.txt","r");
+		h=fopen("users.txt","r");
+		g=fopen("users2.txt","w");
+		
+		/*abre o ficheiro apenas para ler*/
+		/*
+		if(f ==NULL || h==NULL) {printf("Entrou\n");
+			valor=NON_ER;
+		} else {
+			while(fgets(buffer,200,f)!=NULL) {
+				int t=strlen(buffer);
+				buffer[t-2]='\0';
+				buffer[t-1]='\0';
+				Cidade cid;
+				char *token;
+				token=strsep(&buffer,"|");
+				token=strsep(&buffer,"|");
+				memcpy(nome,token,strlen(token)+1);
+				strcpy(citis[i],nome);
+				//citis[i][strlen(nome)]='\0';
+			   // printf("%s\n",citis[i]);
+
+				i++;
+				buffer=b;
+				memset(b,0,200*sizeof(char));
+				
+			}
+			
+			while(fgets(buffer,200,h)!=NULL) {
+				int t=strlen(buffer);
+				buffer[t-2]='\0';
+				buffer[t-1]='\0';
+				Cidade cid;
+				char *token;
+				token=strsep(&buffer,"|");
+				memcpy(nome,token,strlen(token)+1);
+				strncpy(users[o],nome,strlen(token)+1);
+				//users[o][strlen(nome)]='/0';
+				o++;
+				buffer=b;
+				memset(b,0,200*sizeof(char));
+				
+			}
+			//printf("%d\n",i);
+			o--;
+			for (j=0;j<i;j++) {
+				x=rand()%10;
+				//printf("%d\n",x);
+				
+				for(c=0;c<x;c++){
+				        v=rand()%i;
+				        z=rand()%i;
+				        if(z!=v){
+					fprintf(g,"%s|%s|%s|%d|\n",users[rand()%o],citis[z],citis[v],rand()%300);}
+					}
+				
+			}
+			
+			
+
+		}	
+}*/
 /*funcao utilizada para atribuir uma cidade aleatoria aos camioesx
 int atribCity(Trans dest) {
 	char citis[(int)dest->cidz->cidades->tsize][MAXS];
